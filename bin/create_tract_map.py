026b5fc705f0d006ad9aebbe2dc6687c9cc59cbd
Asa Oines <asaoines@gmail.com>
2014-09-18 20:28:28 -0400

    Checkpoint, working on histogram mouseover

--- bin/create_tract_map.py
+++ bin/create_tract_map.py
@@ -4,19 +4,8 @@ import re
 import sys
 import json
 import ogr
-from shapely.geometry import Polygon
-from random import uniform
-import sqlite3
-from globalmaptiles import GlobalMercator
-import zipfile
-from time import time
-import csv
 
-def create_tract_map(input_map_path, tract_list_path, field_name, output_map_path):
-	# Read inputs
-	with open(tract_list_path) as json_file:
-		tracts = json.load(json_file)
-
+def create_tract_map(input_map_path, tracts, state_field_name, state, tract_field_name, output_map_path):
 	# open the geojson file
 	ds = ogr.Open( input_map_path )
 	if ds is None:
@@ -31,8 +20,11 @@ def create_tract_map(input_map_path, tract_list_path, field_name, output_map_pat
 
 	# Get the feature size we'll be mapping in.
 	for i, defn in enumerate( field_defns ):
-		if defn.GetName() == field_name:
-			field_index = i
+		print defn.GetName()
+		if defn.GetName() == tract_field_name:
+			tract_field_index = i
+		if defn.GetName() == state_field_name:
+			state_field_index = i
 
 	geojson_feats = [feat for feat in lyr]
 	feature_collection = {"type": "FeatureCollection",
@@ -40,12 +32,10 @@ def create_tract_map(input_map_path, tract_list_path, field_name, output_map_pat
  											  }
 
 	for feat in geojson_feats:
-		tract = feat.GetField(field_index)
-		if tract in tracts:
+		feat_state = feat.GetField(state_field_index)
+		tract = feat.GetField(tract_field_index)
+		print tract
+		if state == feat_state and tract in tracts:
 			feature_collection["features"].append(feat.ExportToJson())
 	with open(output_map_path, 'w') as f:
 		json.dump(feature_collection, f, ensure_ascii=True)
-
-create_tract_map("../static/data/maps/MA-TRACTS.geojson",
-								 "../static/data/census/cambridge_tracts.json",
-								 "TRACTCE10", "../static/data/maps/CAMBRIDGE-TRACTS.geojson")
